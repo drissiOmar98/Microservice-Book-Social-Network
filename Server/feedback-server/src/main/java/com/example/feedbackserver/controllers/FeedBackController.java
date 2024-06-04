62fdef6021b53d4ca463dd5c07cebef289b13aad
DrissiOmar <omar.drissi@esprit.tn>
2024-06-04 11:55:26 +0100

    Implement security for FeedBackServer

---
+++ Server/feedback-server/src/main/java/com/example/feedbackserver/controllers/FeedBackController.java
@@ -0,0 +1,14 @@
+package com.example.feedbackserver.controllers;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("feedbacks")
+@RequiredArgsConstructor
+@Tag(name = "feedback", description = "Operations related to feedbacks")
+public class FeedBackController {
+
+}
---
+++ Server/feedback-server/src/main/java/com/example/feedbackserver/security/jwt/JwtTokenAuthenticationFilter.java
@@ -0,0 +1,48 @@
+package com.example.feedbackserver.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenAuthenticationFilter  extends OncePerRequestFilter {
+    
+    public static final String HEADER_PREFIX = "Bearer ";
+
+    private final JwtTokenProvider tokenProvider;
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(HEADER_PREFIX)) {
+            return bearerToken.substring(HEADER_PREFIX.length());
+        }
+        return null;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        try {
+            String token = resolveToken(request);
+            if (StringUtils.hasText(token) && this.tokenProvider.validateToken(token)) {
+                Authentication authentication = tokenProvider.getAuthentication(token);
+                if (authentication != null) {
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Could not set user authentication in security context", e);
+        }
+        chain.doFilter(request, response);
+    }
+}
